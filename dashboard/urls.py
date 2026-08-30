6c0b5b0452ea74f9f8e922588f4366c0c49c6980
Abhishek Bujurge <abhishek.bujurge@gmail.com>
2026-08-30 15:30:51 +0530

    Add export URLs for PDF and Excel

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("<int:dataset_id>/", views.view_dashboard, name="view"),
+    path("<int:dataset_id>/export/pdf/", views.export_pdf, name="export_pdf"),
+    path("<int:dataset_id>/export/excel/", views.export_excel, name="export_excel"),
 ]
